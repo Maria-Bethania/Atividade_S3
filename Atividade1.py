3027d911819985c2f68ba6560d82189937b02a29
Maria Betânia <mariabetaniapaivanunes99@gmail.com>
2024-04-16 16:04:07 -0500

    Resolução_Atividade1

---
+++ Atividade1.py
@@ -0,0 +1,21 @@
+# Descrevendo atividades do dia-a-dia condicionadas
+# Atividade escolhida: Cozinhar o feijão
+
+# Escolho o tipo de feijão que vou cozinhar 
+# Decido a quantidade que vou cozinhar, levando em conta o número de pessoas que irão comer.
+# Separo o feijão, retirando grãos estragados ou impurezas.
+# Lavo em água corrente.
+
+# SE queroreduzir o tempo de cozimento:
+#   Deixo o feijão de molho em água por algumas horas.
+# SENÃO:
+#   Prossigo para o cozimento do feijão.
+
+# Tempero o feijão, adicionando alho, cebola, louro, sal e pimenta.
+# Refogo os temperos ao óleo e manteigga em uma panela de pressão.
+# Coloco o feijão na panela de pressão, junto aos temperos.
+# Adiciono água suficiente para cobrir o feijão.
+# Cozinhe o feijão por cerca de 20 a 30 minutos após pegar pressão.
+
+# Verifico se o feijão está macio e bem cozido.
+# Sirvo o feijão.
